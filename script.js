8efcae9d3e92d73fb8a438cb4de8c86b9a090072
lbackman <lucasbackman@gmail.com>
2024-03-11 15:43:00 +0200

    Implement rudimentary gameplay

--- script.js
+++ script.js
@@ -15,9 +15,11 @@ const gameBoard = (function (rows, columns) {
       console.log('Out of bounds')
     } else if (board[y][x] === ' ') {
       board[y][x] = marker;
+      return true;
     } else {
       console.log('Occupied slot');
     }
+    return false;
   };
 
   const currentBoard = function () {
@@ -26,3 +28,40 @@ const gameBoard = (function (rows, columns) {
 
   return { placeMarker, currentBoard }
 })(3, 3);
+
+const player = function (name, marker){
+  return { name, marker }
+};
+
+const playerOne = player("Player 1", "X");
+const playerTwo = player("Player 2", "O");
+
+const ticTacToe = (function (player1, player2, board) {
+
+  const players = [player1, player2]
+  let currentPlayerIndex = 0;
+
+  const changePlayerIndex = function (playerIndex) {
+    return 1 - playerIndex;
+  };
+
+  const playRound = function () {
+    const y = input = prompt("Which row?");
+    const x = input = prompt("Which column?");
+    if (board.placeMarker(players[currentPlayerIndex].marker, x, y)) {
+      currentPlayerIndex = changePlayerIndex(currentPlayerIndex);
+      console.log(`Current player: ${players[currentPlayerIndex].marker}`);
+    } else {
+      alert("Try again");
+    };
+  };
+
+  const playGame = function () {
+    while (true) {
+      playRound();
+      console.log(board.currentBoard());
+    };
+  };
+
+  return { playGame };
+})(playerOne, playerTwo, gameBoard);
